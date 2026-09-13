0407956871c3d1f0bea617e147d54e7670d49507
Juanchuchuy <juanignaciolinares2013@gmail.com>
2026-09-13 15:59:19 -0300

    Actualizado para adaptar funcionalidades

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 
+import { Routes, Route } from 'react-router-dom'
 import CardSection from './components/CardSection'
 import Header from './components/Header'
-import {Bus} from 'lucide-react'
+import Horarios from './pages/Horarios'
 import './App.css'
 
 function App() {
   return (
     <>
       <Header></Header>
-      <CardSection />
+      <Routes>
+        <Route path="/" element={<CardSection />} />
+        <Route path="/horarios/:lineaId" element={<Horarios />} />
+      </Routes>
     </>
   )
 }
